ec217e33f76caeb5e713361e0482adc733390e14
Jian Zhou <i@janzhou.org>
2017-01-20 00:19:05 -0500

    add borrow thread

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use rary::english::hello;
 mod mod_example;
 use mod_example::*;
 
+mod thread_example;
+use thread_example::*;
+
+mod borrow;
+use borrow::*;
+
 fn main() {
     println!("{}", hello());
 
     mod_example();
+    borrow();
+    thread_example();
 }
